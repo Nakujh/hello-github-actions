99abe1e111591fe38e6c4fc2e9bdc8b75be09427
nj185051 <nj185051@ncr.com>
2021-01-19 23:03:12 -0800

    Commit add LineAppender demo

---
+++ LineAppender/Build/AppendLine.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def get_current_date_time():
+    dtCurrent = datetime.now()
+    return dtCurrent.strftime('%d/%m/%Y %H:%M:%S')
+
+def append_new_line(sFileName, sTextToAppend):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    FileObject = open(sFileName, "a+")
+    # Move read cursor to the start of file.
+    FileObject.seek(0)
+    # If file is not empty then append '\n'
+    Data = FileObject.read(100)
+    if len(Data) > 0:
+        FileObject.write("\n")
+    # Append text at the end of file
+    FileObject.write(f'// {sTextToAppend} ({get_current_date_time()})')
+    
+append_new_line('..\ThreadExample\ThreadProject.dpr', 'Runner Machine note:  Release: ')
---
+++ LineAppender/ThreadExample/ThreadProject.dpr
@@ -0,0 +1,20 @@
+program ThreadProject;
+
+uses
+  Vcl.Forms,
+  Unit2 in 'Unit2.pas' {Form2},
+  Unit1 in 'Unit1.pas',
+  PingU in 'PingU.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
+// Site Builder note:  Release: 
+// Runner Machine note:  Release:  (2021-01-19 22:47:37.305230)
+// Runner Machine note:  Release:  (19/01/2021 22:50:52)
+// Runner Machine note:  Release:  (19/01/2021 22:56:27)
